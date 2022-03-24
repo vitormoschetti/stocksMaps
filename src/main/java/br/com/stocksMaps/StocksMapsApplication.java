63bbdd2f4646d73d06ded1431d1051562f638fac
Vitor Moschetti <vitormoschetti@terra.com.br>
2022-03-23 21:13:39 -0300

    primeiro commit

---
+++ src/main/java/br/com/stocksMaps/StocksMapsApplication.java
@@ -0,0 +1,13 @@
+package br.com.stocksMaps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StocksMapsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StocksMapsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/stocksMaps/StocksMapsApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.stocksMaps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StocksMapsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
