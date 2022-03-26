ae6a7ba8b4372d8b49ef481256b2670cf27dcc60
vitor Alexandre Moschetti <vitor.alexandre.moschetti@everis.nttdata.com>
2022-03-26 10:53:47 -0300

    Adicionando JPA

--- src/main/java/br/com/stocksmaps/core/crudOperations/IBuscarTodos.java
+++ src/main/java/br/com/stocksmaps/core/crudOperations/IBuscarTodos.java
@@ -1,7 +1,11 @@
 package br.com.stocksmaps.core.crudOperations;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
+
 public interface IBuscarTodos<E> {
 
-//    Page<E> buscarTodos(Specification<E> spec, Pageable pageable);
+    Page<E> buscarTodos(Specification<E> spec, Pageable pageable);
 
 }
