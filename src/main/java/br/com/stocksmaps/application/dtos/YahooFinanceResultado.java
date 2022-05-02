ba817906d8a9cda34e67d6054bdfe0f35fc9efd8
vitor Alexandre Moschetti <vitor.alexandre.moschetti@everis.nttdata.com>
2022-05-02 16:42:16 -0300

    integracao com api yahoo finance

--- src/main/java/br/com/stocksmaps/application/dtos/YahooFinanceResultado.java
+++ src/main/java/br/com/stocksmaps/application/dtos/YahooFinanceResultado.java
@@ -1,28 +1,16 @@
 package br.com.stocksmaps.application.dtos;
 
-import com.fasterxml.jackson.annotation.JsonAnyGetter;
-import com.fasterxml.jackson.annotation.JsonAnySetter;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
 
-import java.util.Collections;
-import java.util.Map;
-
+@Getter
 @ToString
 @NoArgsConstructor
 public class YahooFinanceResultado {
 
-    private Map<Long, YahooFinanceAtivoDTO> acoes;
-
-    @JsonAnySetter
-    public void put(Long key, YahooFinanceAtivoDTO value) {
-        this.acoes = Collections.singletonMap(key, value);
-    }
-
-    @JsonAnyGetter
-    public Map<Long, YahooFinanceAtivoDTO> getAcoes() {
-        return this.acoes;
-    }
+    @JsonProperty("language")
+    private String precoAtual;
 
 }
--- src/main/java/br/com/stocksmaps/infra/adapters/yahooFinance/YahooFinanceRequestInterceptor.java
+++ src/main/java/br/com/stocksmaps/infra/adapters/yahooFinance/YahooFinanceRequestInterceptor.java
@@ -5,8 +5,6 @@ import feign.RequestTemplate;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import java.util.Map;
-
 @Component
 public class YahooFinanceRequestInterceptor implements RequestInterceptor {
 
